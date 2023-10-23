719e2b6e755a78df56a4152286e3586f950cebe7
qqsongyang <719717747@qq>
2023-10-23 17:07:07 +0800

    更新check12

---
+++ Assets/3Plugins/QToolsKit/UIFramework/Editor/Export/ExportCodeUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using _3Plugins.QToolsKit.UIFramework.Editor.Window.Data;
+using _3Plugins.QToolsKit.UIFramework.Editor.Window.Data.Json;
+using UnityEditor;
+using UnityEngine;
+
+namespace _3Plugins.QToolsKit.UIFramework.Editor.Export
+{
+    public  class ExportCodeUtil
+    {
+        public TextAsset s1;
+        public static void Excute(PrefabCacheData data)
+        {
+            //文件路径
+            string mUIClassName = FormWindowData.WindowData.generateParentDirectoryPath;
+            string scriptPath = $"{mUIClassName}.cs";
+            //上下文
+            string context = "classname";
+
+            FileStream file = new FileStream(scriptPath, FileMode.CreateNew);
+            StreamWriter fileW = new StreamWriter(file, System.Text.Encoding.UTF8);
+            fileW.Write(context);
+            fileW.Flush();
+            fileW.Close();
+            file.Close();
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
